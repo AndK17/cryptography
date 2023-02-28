cad1194a897348c3250ee39b1c6d6280efd73ec1
AndK <karpukhin_av@mail.ru>
2023-02-28 23:17:54 +0300

    2 done

--- hill/ranalysis.py
+++ hill/ranalysis.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+import itertools
+
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
@@ -16,7 +18,6 @@ def int_to_str(s):
 
 
 def invert_alpha(a):
-    print(a)
     if a == 0:
         return 0
     n = 0
@@ -33,7 +34,6 @@ def invert_matrix(matrix):
     m_size = len(matrix)
     det = round(np.linalg.det(matrix))%26
     if math.gcd(det, 26) != 1:
-        print(det)
         return 'Bad matrix'
     
     inv_det = invert_alpha(det)
@@ -70,29 +70,25 @@ def get_m(pairs):
         print(matr)
     return matr
     
-import itertools
-pairs = [([7, 4, 11], [15, 15, 3]), ([8, 0, 12], [20, 4, 6]), ([2, 0, 13], [3, 2, 2]), ([24, 4, 18], [4, 8, 8]), ([13, 8, 2], [0, 1, 17]),
-         ([12, 24, 13], [1, 4, 24]), ([18, 4, 17], [3, 24, 0]), ([1, 0, 18], [20, 21, 11]), ([1, 3, 5], [13, 20, 11])]
-for i in itertools.combinations(pairs, len(pairs[0][0])):
-    o_text = []
-    c_text = []
-    for j in i:
-        o_text.append(j[0])
-        c_text.append(j[1])
-    
-    print(np.array(o_text).T)
-    print(invert_matrix(np.array(o_text).T))
-    print()
+
+pairs1 = [([7, 4, 11], [15, 15, 3]), ([12, 24, 13], [1, 4, 12]), ([2, 0, 13], [3, 2, 2]), ([1, 3, 5], [13, 20, 11])]
+pairs2 = [([11, 14, 25], [21, 19, 12]), ([1, 12, 4], [4, 9, 15]), ([24, 14, 20], [4, 24, 16]), ([17, 25, 25], [18, 12, 1])]
+
 if __name__ == '__main__':
-    pass
-    # print(get_m(pairs))
-    
-    # key = get_key(r'ot.txt')
-    # print(key)
-    # key = invert_matrix(key)
-    # print(key)
-    
-    # t = get_key(r't.txt')
-    
-    
-    # print(t.dot(key)%26)
+    pairs = pairs2
+    for i in itertools.combinations(pairs, len(pairs[0][0])):
+        o_text = []
+        c_text = []
+        for j in i:
+            o_text.append(j[0])
+            c_text.append(j[1])
+        
+        
+        m = invert_matrix(np.array(o_text).T)
+        if m != 'Bad matrix':
+            print(np.array(o_text).T)
+            print(np.array(c_text).T)
+            print('NICE')
+            print(m)
+            print(np.array(c_text).T.dot(m)%26)
+        print()
